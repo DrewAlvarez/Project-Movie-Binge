4077c7ef3b4bfdb0a098da9ce2657a2f8c868955
Ashley Hunt <ashhunt07@gmail.com>
2020-08-29 13:28:22 -0500

    adjusted slider and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,28 @@ var movieDiv = $("<div>")
 
 
 for(var i=0; i < 10; i++){
-  var randomId = Math.floor(Math.random()*6534893)
-  var moviePoster = "0000000" + randomId;
-  
-  posterFunc(moviePoster.substr(moviePoster.length-7))
+    var randomId = Math.floor(Math.random()*6534893)
+    var moviePoster = "0000000" + randomId;
+
+    posterFunc(moviePoster.substr(moviePoster.length-7))
 }
 
 function posterFunc(moviePoster){
-  var omdbUrl = "https://www.omdbapi.com/?i=tt" + moviePoster + "&apikey=f52998b5";
+    var omdbUrl = "https://www.omdbapi.com/?i=tt" + moviePoster + "&apikey=f52998b5";
 
-  $.ajax({
-      url: omdbUrl,
-      method: "GET",
+$.ajax({
+    url: omdbUrl,
+    method: "GET",
 
-  }).then(function(response) {
-      console.log(response)
-      
-      if(response.Response === "True" && response.Type === "movie" && response.Poster !== "N/A"){
+    }).then(function(response) {
+    console.log(response)
+
+    if(response.Response === "True" && response.Type === "movie" && response.Poster !== "N/A"){
         console.log(response);
         imgSrc = response.Poster
         var randomMovies = $("<img>").attr("src", imgSrc);
         $("#movie-scroll").append(randomMovies)
-      }
-  });
+    }
+    
+    });
 }
