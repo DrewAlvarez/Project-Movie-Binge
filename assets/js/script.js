9d6552adbfaeba0a4677a99869253543a49e0129
PureDemon <umaga1031@yahoo.com>
2020-08-29 13:17:57 -0500

    j

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,28 @@ var movieList = ["Back to the Future", "Jumanji", "Jurassic Park", "Underworld",
 var movieDiv = $("<div>")
 
 
-for(var i=0; i < 10; i++){
-  var randomId = Math.floor(Math.random()*6534893)
+for (var i = 0; i < 10; i++) {
+  var randomId = Math.floor(Math.random() * 6534893)
   var moviePoster = "0000000" + randomId;
-  
-  posterFunc(moviePoster.substr(moviePoster.length-7))
+
+  posterFunc(moviePoster.substr(moviePoster.length - 7))
 }
 
-function posterFunc(moviePoster){
+function posterFunc(moviePoster) {
   var omdbUrl = "https://www.omdbapi.com/?i=tt" + moviePoster + "&apikey=f52998b5";
 
   $.ajax({
-      url: omdbUrl,
-      method: "GET",
+    url: omdbUrl,
+    method: "GET",
 
-  }).then(function(response) {
-      console.log(response)
-      
-      if(response.Response === "True" && response.Type === "movie" && response.Poster !== "N/A"){
-        console.log(response);
-        imgSrc = response.Poster
-        var randomMovies = $("<img>").attr("src", imgSrc);
-        $("#movie-scroll").append(randomMovies)
-      }
+  }).then(function (response) {
+    console.log(response)
+
+    if (response.Response === "True" && response.Type === "movie" && response.Poster !== "N/A") {
+      console.log(response);
+      imgSrc = response.Poster
+      var randomMovies = $("<img>").attr("src", imgSrc);
+      $("#movie-scroll").append(randomMovies)
+    }
   });
 }
