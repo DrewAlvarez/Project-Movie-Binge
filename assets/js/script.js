5ac63222afa891320ec60b5636ef6ae41182c025
Drew A <alvarez.drew@gmail.com>
2020-08-29 09:31:51 -0500

    solved the image rendering issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,24 @@
 var movieList = ["Back to the Future", "Jumanji", "Jurassic Park", "Underworld", "The Princess Bride", "City Slickers", "Smokey and the Bandit", "Tropic Thunder", "Dumb and Dumber", "Excalibur"]
 var movieDiv = $("<div>")
-for(i=0; i < movieList.length; i++){
-  moviePoster = movieList[i];
+
+for(i=0; i <movieList.length; i++){
+  moviePoster = movieList[i]
+
   posterFunc(moviePoster)
-      
 }
 
 function posterFunc(moviePoster){
 
-  var omdbUrl = "https://www.omdbapi.com/?t=" + moviePoster + "&apikey=trilogy";
+  var omdbUrl = "https://www.omdbapi.com/?t=" + moviePoster + "&apikey=f52998b5";
 
   $.ajax({
       url: omdbUrl,
       method: "GET",
 
   }).then(function(response) {
-      console.log(response);
-      
-      var randomMovies = $("<img>")
+      console.log(response.Poster);
       imgSrc = response.Poster
-      $("img").attr("src", imgSrc);
-      $("img").attr("data-movie", i)
+      var randomMovies = $("<img>").attr("src", imgSrc);
       movieDiv.append(randomMovies)
       $("body").append(movieDiv)
   });
